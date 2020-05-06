444d7a15bdc703fd22c2f4fcbb3e4bfaa879ee4d
unknown <robdjmixeo@gmail.com>
2020-05-05 19:44:59 -0500

    make it better 2

---
+++ Server/config/config.js
@@ -0,0 +1,50 @@
+//==============================
+// puerto
+//==============================
+
+process.env.PORT = process.env.PORT || 8080;
+
+
+
+// =======================================
+//    CLOUDINARY
+// =====================================
+process.env.CLOUDINARY_CLOUD_NAME = 'dsifghbyk';
+process.env.CLOUDINARY_APY_KEY = '826663455555642';
+process.env.CLOUDINARY_APY_SECRET = 'aHfpzsAd2hw8cZwK7vMcaEk7Dyg';
+
+
+
+//==============================
+// entorno
+//==============================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+//==============================
+// BASE DA DATOS
+//==============================
+let urlDB;
+
+// if (process.env.NODE_ENV === 'dev') {
+//     urlDB = 'mongodb://localhost/tienda';
+// } else {
+// }
+
+urlDB = process.env.MONGODB_URI;
+//==============================
+// VENCIMIENTO DEL TOKEN
+//==============================
+// 60 SEGUNDOS
+// 60 MINUTOS
+// 24 HORAS
+// 30 DIAS
+
+process.env.CADUCIDAD_TOKEN = '48h';
+
+//==============================
+// SEED O SEMILLA DE AUTENTICACION
+//==============================
+process.env.SEED = process.env.SEED || 'este-es-el-seed-desarrollo';
+
+
+process.env.URLDB = urlDB;
